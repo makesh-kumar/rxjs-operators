912246affe18be7e6f7ed458711b439cd117513f
makesh-kumar <mynameismakesh@gmail.com>
2021-12-05 19:19:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-cwvn3w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-cwvn3w)
---
+++ index.ts
@@ -0,0 +1,191 @@
+import './style.css';
+
+import {
+  of,
+  map,
+  mapTo,
+  Observable,
+  forkJoin,
+  merge,
+  interval,
+  from,
+  fromEvent,
+  fromEventPattern,
+  range,
+  timer,
+  iif,
+  groupBy,
+  concat,
+  race,
+} from 'rxjs';
+import {
+  buffer,
+  bufferCount,
+  bufferTime,
+  bufferWhen,
+  count,
+  debounce,
+  debounceTime,
+  delay,
+  expand,
+  max,
+  mergeMap,
+  min,
+  reduce,
+  switchMap,
+} from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
+/** Creation operators
+ * ajax, interval, of, from, fromEvent, fromEventPattern, range, throwErrro, timer, iif
+ */
+
+// let obs = ajax('https://jsonplaceholder.typicode.com/posts');
+
+// let obs = interval(2000);
+
+// let obs = of(1, 2, 3, 4);
+
+// let obs = from(new Set().add(1));
+
+// let obs = fromEvent(document, 'click');
+
+// let obs = fromEventPattern(foo,removeClickHandler);
+
+// let obs = range(10, 20);
+
+// let obs = timer(1000);
+
+// let obs = iif(() => false, of('True Case'), of('false Case'));
+
+// let obs = throwError(new Error('error occurred'));
+
+// obs.subscribe(
+//   (d) => {
+//     console.log(d);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+/*********************************************** */
+
+/** Mathematical operators
+ * count, max, min, reduce,
+ */
+
+// let obs = of(2, 3, 4, 5, 6).pipe(
+//   count((val) => {
+//     return val % 2 === 0; //3
+//   })
+// );
+
+// let obs = of(2, 3, 4, 5, 6).pipe(max());
+
+// let obs = of(2, 3, 4, 5, 6).pipe(min());
+
+// let obs = of(2, 3, 4, 5, 6).pipe(
+//   reduce((a, b) => {
+//     return a + b;
+//   }, 10)
+// );
+
+// obs.subscribe((d) => {
+//   console.log(d);
+// });
+
+/******************************************************* */
+
+/**Transformation operator
+ *
+ * buffer, bufferCount, bufferTime, bufferToggle, bufferWhen,
+ * expand, groupBy,
+ * map, mapTo, switchMap,
+ * window
+ */
+
+// let a = fromEvent(document, 'click');
+// let obs = interval(1000).pipe(buffer(a));
+
+// let obs = interval(1000).pipe(bufferCount(2));
+
+// let obs = interval(1000).pipe(bufferTime(2000));
+
+// let obs = interval(1000).pipe(bufferWhen(()=> {
+//   return interval(2000)
+// }))
+
+// let obs = of(1, 2, 3, 4).pipe(
+//   expand((d) => {
+//     return of(d * 2);
+//   })
+// );
+
+// let obj = [
+//   { name: 'makesh', type: 'a' },
+//   { name: 'kumar', type: 'b' },
+//   { name: 'john', type: 'a' },
+// ];
+// let obs = from(obj).pipe(
+//   groupBy((item) => item.type),
+//   map((d) => {
+//     return d;
+//   })
+// );
+
+// let obs = of(1, 2, 3, 4).pipe(
+//   map((d) => {
+//     return d * 10;
+//   })
+// );
+
+// let obs = of(1, 2, 3, 4).pipe(mapTo(8));
+
+// let obs = of(1, 2, 3, 4, 5).pipe(
+//   switchMap((d) => {
+//     return ajax('https://jsonplaceholder.typicode.com/posts');
+//   })
+// );
+
+// obs.subscribe((d) => {
+//   console.log(d);
+// });
+
+/************************************************* */
+
+/**
+ * Join operators
+ *
+ * concat, forkJoin, merge, race
+ */
+
+// let obs1 = of(1, 2, 3, 4).pipe(delay(2000));
+// let obs2 = of(5, 6, 7, 8);
+
+// let obs = race(obs1, obs2);
+
+// obs.subscribe((d) => {
+//   console.log(d);
+// });
+
+/****************************************************************** */
+
+/**
+ * Filtering operator
+ *
+ * debounce, debounceTime,
+ * distinct, elementAt,
+ * filter, first,last,
+ * ignoreElements, sample, skip, throttle
+ */
+
+let obs = fromEvent(document, 'click').pipe(
+  debounce(() => {
+    return interval(2000);
+  })
+);
+
+obs.subscribe((d) => {
+  console.log(d);
+});
